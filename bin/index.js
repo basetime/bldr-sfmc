ad79fa0c30ca3665f56bccd03367674837dd3eb0
Tony Zupancic <tony.zupancic@avagate.com>
2022-04-14 13:34:49 -0500

    updated comments

--- bin/index.js
+++ bin/index.js
@@ -34,27 +34,52 @@ const init = async (req, argv, store) => {
     } else {
         let blueprint;
 
-        // Route all requests
         switch (req) {
+            /**
+             * Config route handles all CLI configuration of SFMC Instance\
+             * See README for config details
+             */
+
             case 'config':
                 configSwitch.switch(req, argv, store);
                 break;
+
+            /**
+             * Add files in bulk or by filepath to the stash.json file
+             */
             case 'add':
                 blueprint = await blueprintInit.set(null, store);
                 addSwitch.switch(req, argv, blueprint, store);
                 break;
+
+            /**
+             * Works with Stash operations
+             */
             case 'stash':
                 blueprint = await blueprintInit.set(null, store);
                 stashSwitch.switch(req, argv, blueprint, store);
                 break;
+
+            /**
+             * Displays current State and Staged Files
+             */
             case 'status':
                 blueprint = await blueprintInit.set(null, store);
                 statusSwitch.switch(req, argv, blueprint, store);
                 break;
+
+            /**
+             * Format and push Staged files into current State SFMC Instance
+             */
             case 'push':
                 blueprint = await blueprintInit.set(null, store);
                 pushSwitch.switch(req, argv, blueprint, store);
                 break;
+
+            /**
+             * Default handles all context specific routes
+             * Context specific routes are located in utils/Blueprint/context/:context
+             */
             default:
                 blueprint = await blueprintInit.set(null, store);
                 contextSwitch.switch(req, argv, blueprint, store);
